13d658075a7e5ee21602d4a95f3289e0703d56c2
Yao Zongyou <yaozy2010@gmail.com>
2015-06-13 17:20:04 +0800

    add UIListViewTest

---
+++ ListViewTest/src/main/java/com/yaozongyou/listviewtest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.yaozongyou.listviewtest;
+
+import java.util.List;
+import java.util.ArrayList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends Activity
+{
+    /*
+    private String[] data = {
+        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"
+    };
+    */
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
+
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        /*
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        */
+
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Fruit fruit = fruitList.get(position);
+                        Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void initFruits() {
+        Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+        fruitList.add(apple);
+        Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+        fruitList.add(banana);
+        Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+        fruitList.add(orange);
+        Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+        fruitList.add(watermelon);
+        Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+        fruitList.add(pear);
+        Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+        fruitList.add(grape);
+        Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+        fruitList.add(pineapple);
+        Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+        fruitList.add(strawberry);
+        Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+        fruitList.add(cherry);
+        Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+        fruitList.add(mango);
+    }
+}
