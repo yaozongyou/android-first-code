ffd48418e32ab54a802e41b587bb4f5703b075a3
Yao Zongyou <yaozy2010@gmail.com>
2015-08-30 00:28:23 +0800

    add files

--- BroadcastTest/src/main/java/com/yaozongyou/broadcasttest/MainActivity.java
+++ BroadcastTest/src/main/java/com/yaozongyou/broadcasttest/MainActivity.java
@@ -12,11 +12,16 @@ import android.widget.Toast;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.support.v4.content.LocalBroadcastManager;
 
 public class MainActivity extends Activity
 {
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
+
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+    
 
     /** Called when the activity is first created. */
     @Override
@@ -25,26 +30,45 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
                 Intent intent = new Intent("com.yaozongyou.broadcasttest.MY_BROADCAST");
-                sendBroadcast(intent);
+                sendOrderedBroadcast(intent, null);
+                */
+                Intent intent = new Intent("com.yaozongyou.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
             }        
         });
 
-
+        /*
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
+        */
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.yaozongyou.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        //unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
+        }
     }
 
     class NetworkChangeReceiver extends BroadcastReceiver {
