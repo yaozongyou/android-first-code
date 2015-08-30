7275bc3f10ced18f3cf6a87cb944ba2f15aba368
Yao Zongyou <yaozy2010@gmail.com>
2015-08-30 15:22:27 +0800

    add files

---
+++ SharedPreferencesTest/src/main/java/com/yaozongyou/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.yaozongyou.sharedpreferencestest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.util.Log;
+import android.content.SharedPreferences;
+
+public class MainActivity extends Activity
+{
+    private Button saveData;
+    private Button restoreData;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        saveData = (Button)findViewById(R.id.save_data);
+        restoreData = (Button)findViewById(R.id.restore_data);
+            
+        saveData.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                        editor.putString("name", "Tom");
+                        editor.putInt("age", 28);
+                        editor.putBoolean("married", false);
+                        editor.commit();
+                    }
+                });
+
+        restoreData.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                        String name = pref.getString("name", "");
+                        int age = pref.getInt("age", 0);
+                        boolean married = pref.getBoolean("married", false);
+                        Log.d("MainActivity", "name is " + name);
+                        Log.d("MainActivity", "age is " + age);
+                        Log.d("MainActivity", "married is " + married);
+                    }
+                });
+    }
+}
